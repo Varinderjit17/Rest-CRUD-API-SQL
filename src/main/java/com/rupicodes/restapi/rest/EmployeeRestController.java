0d49969e50e02e3df7c29d9ebf36a446cc13da36
rupi <varinderjit17@gmail.com>
2024-09-08 11:47:41 +1200

    CRUD methods added to Service

--- src/main/java/com/rupicodes/restapi/rest/EmployeeRestController.java
+++ src/main/java/com/rupicodes/restapi/rest/EmployeeRestController.java
@@ -4,9 +4,7 @@ import com.rupicodes.restapi.dao.EmployeeDAO;
 import com.rupicodes.restapi.entity.Employee;
 import com.rupicodes.restapi.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,5 +22,30 @@ public class EmployeeRestController {
     public List<Employee> findAllEmployees() {
         List<Employee> employees= employeeService.findAllEmployee();
         return employees;
+    }
+
+    @GetMapping("/employees/{employeeId}")
+    public Employee findEmployee(@PathVariable int employeeId) {
+        Employee employee = employeeService.findEmployeeById(employeeId);
+        return employee;
+    }
+
+    @PostMapping("/employees")
+    public Employee saveEmployee(@RequestBody Employee employee) {
+        employee.setId(0);
+        Employee dbEmployee= employeeService.saveEmployee(employee);
+        return dbEmployee;
+    }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee) {
+        Employee dbEmployee = employeeService.saveEmployee(employee);
+        return dbEmployee;
+    }
+
+    @DeleteMapping("/employees/{employeeId}")
+    public String deleteEmployee(@PathVariable int employeeId) {
+        employeeService.deleteEmployeeById(employeeId);
+        return "Employee deleted";
     }
 }
