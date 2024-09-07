326913d22a1211a2d9705819e225d10d31b20363
rupi <varinderjit17@gmail.com>
2024-09-07 12:13:04 +1200

    CRUD methods added to Service

--- src/main/java/com/rupicodes/restapi/service/EmployeeServiceImpl.java
+++ src/main/java/com/rupicodes/restapi/service/EmployeeServiceImpl.java
@@ -4,6 +4,7 @@ import com.rupicodes.restapi.dao.EmployeeDAO;
 import com.rupicodes.restapi.entity.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -21,5 +22,22 @@ public class EmployeeServiceImpl implements EmployeeService {
     public List<Employee> findAllEmployee() {
         List<Employee> employees = employeeDAO.findAllEmployee();
         return employees;
+    }
+
+    @Override
+    public Employee findEmployeeById(int employeeId) {
+        return employeeDAO.findEmployeeById(employeeId);
+    }
+
+    @Transactional
+    @Override
+    public Employee saveEmployee(Employee employee) {
+        return employeeDAO.saveEmployee(employee);
+    }
+
+    @Transactional
+    @Override
+    public void deleteEmployeeById(int employeeId) {
+        employeeDAO.deleteEmployeeById(employeeId);
     }
 }
